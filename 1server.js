9f253f132b83852dc5d765f6c09ff0be56b6ba01
Rida Noor <rida.noor@gmail.com>
2026-02-06 00:07:59 +0500

    serverjsfix

--- 1server.js
+++ 1server.js
@@ -2,7 +2,8 @@ const next = require("next");
 const http = require("http");
 
 const port = process.env.PORT || 3000;
-const app = next({ dev: true });
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
